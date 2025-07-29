912fd24389b1040a41b55badd658ef1f42eae3b5
boki <lsb530@naver.com>
2025-07-30 00:43:58 +0900

    리액트 서버 컴포넌트 주의사항 - Client컴포넌트에서 Server컴포넌트를 배치하면 자동으로 Server컴포넌트를 Client로 변환

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,5 +1,11 @@
 import styles from "./page.module.css";
+import ClientComponent from "@/app/(with-searchbar)/client-component";
 
 export default function Home() {
-  return <div className={styles.page}>인덱스 페이지</div>
+  return (
+    <div className={styles.page}>
+      인덱스 페이지
+      <ClientComponent />
+    </div>
+  )
 }
