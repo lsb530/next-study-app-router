f8eaceff06b4ca94f297e7af0f13d2141ffc781d
boki <lsb530@naver.com>
2025-07-30 01:05:03 +0900

    네비게이팅 - 동적(programmatic) 이동

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,18 +1,24 @@
 "use client"
 
 import {ChangeEvent, useState} from "react";
+import {useRouter} from "next/navigation";
 
 export default function Searchbar() {
+  const router = useRouter()
   const [search, setSearch] = useState("")
 
   const onChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value)
   }
 
+  const onSubmit = () => {
+    router.push(`/search?q=${search}`)
+  }
+
   return (
     <div>
-      <input value={search} onChange={onChangeSearch} />
-      <button>검색</button>
+      <input value={search} onChange={onChangeSearch}/>
+      <button onClick={onSubmit}>검색</button>
     </div>
   )
 }
