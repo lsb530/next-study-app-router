56b3c94a33f3286e8ea1089ad63eaaa3c65b017a
boki <lsb530@naver.com>
2025-07-30 13:13:13 +0900

    데이터 캐시(Data Cache) - no-store/force-cache/revalidate/tags

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -3,9 +3,10 @@ import style from "./page.module.css";
 import books from "@/mock/books.json";
 import {BookData} from "@/types";
 
-async function AllBooks () {
+async function AllBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    {cache: "no-store"}
   )
   if (!response.ok) {
     return <div>오류가 발생했습니다 ...</div>
@@ -20,9 +21,13 @@ async function AllBooks () {
   </div>
 }
 
-async function RecoBooks () {
+async function RecoBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    // { cache: "no-store" }
+    // { cache: "force-cache" }
+    // {next: { tags: ['a'] }}
+    {next: { revalidate: 3 }} // 3초만 캐싱
   )
   if (!response.ok) {
     return <div>오류가 발생했습니다 ...</div>
@@ -42,7 +47,7 @@ export default async function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks />
+        <RecoBooks/>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
