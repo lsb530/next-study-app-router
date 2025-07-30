42c52815608e3feaaa5957c3589102154e82800a
boki <lsb530@naver.com>
2025-07-30 12:48:28 +0900

    앱 라우터의 데이터 페칭 - 전체도서/추천도서 가져오기

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,48 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import books from "@/mock/books.json";
+import {BookData} from "@/types";
 
-export default function Home() {
+async function AllBooks () {
+  const response = await fetch(`http://localhost:12345/book`)
+  if (!response.ok) {
+    return <div>오류가 발생했습니다 ...</div>
+  }
+
+  const allBooks: BookData[] = await response.json()
+
+  return <div>
+    {allBooks.map((book) => (
+      <BookItem key={book.id} {...book} />
+    ))}
+  </div>
+}
+
+async function RecoBooks () {
+  const response = await fetch(`http://localhost:12345/book/random`)
+  if (!response.ok) {
+    return <div>오류가 발생했습니다 ...</div>
+  }
+
+  const recoBooks: BookData[] = await response.json()
+
+  return <div>
+    {recoBooks.map((book) => (
+      <BookItem key={book.id} {...book} />
+    ))}
+  </div>
+}
+
+export default async function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks/>
       </section>
     </div>
   );
