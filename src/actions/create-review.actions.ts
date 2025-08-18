e149b8e5e7c28fbb4d3cf06043f8405bca1a8f20
boki <lsb530@naver.com>
2025-08-18 12:41:37 +0900

    클라이언트 컴포넌트에서의 서버 액션

--- src/actions/create-review.actions.ts
+++ src/actions/create-review.actions.ts
@@ -2,24 +2,33 @@
 
 import {revalidateTag} from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(
+  _: any,
+  formData: FormData
+) {
   const bookId = formData.get('bookId')?.toString();
   const content = formData.get('content')?.toString();
   const author = formData.get('author')?.toString();
 
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요"
+    }
   }
 
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
+      // `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/1`, // 에러 검증
       {
         method: 'POST',
         body: JSON.stringify({bookId, content, author}),
       },
-    );
-    console.log(response.status);
+    )
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
     /*
       Server Action 또는 Server Component 에서만 사용 가능
       캐시도 모두 Purge-무효화함(Data Cache + Full Route Cache)
@@ -42,8 +51,14 @@ export async function createReviewAction(formData: FormData) {
 
     // 5. 태그 기준, 데이터 캐시 재검증(효율적인 방법)
     revalidateTag(`review-${bookId}`)
+    return {
+      status: true,
+      error: ""
+    }
   } catch (err) {
-    console.error(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다: ${err}`
+    }
   }
 }
