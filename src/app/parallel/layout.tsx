f9db0fed10805b9bbcf653834f9e225de12e07b4
boki <lsb530@naver.com>
2025-08-21 01:17:28 +0900

    병렬 라우트(parallel routes)

---
+++ src/app/parallel/layout.tsx
@@ -0,0 +1,14 @@
+import {ReactNode} from "react";
+
+export default function Layout(
+  {children, sidebar}: {
+    children: ReactNode, sidebar: ReactNode
+  }
+) {
+  return (
+    <div>
+      {sidebar}
+      {children}
+    </div>
+  )
+}
